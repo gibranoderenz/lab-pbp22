764d0cec197918a236ef1f304cd0879f8ca3c287
gibranoderenz <gibranoderenz123@gmail.com>
2022-10-06 14:35:31 +0700

    Do Lab 5 assignment

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path
-from wishlist.views import show_wishlist, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user
+from wishlist import views
 
 app_name = "wishlist"
 
 urlpatterns = [
-    path('', show_wishlist, name='show_wishlist'),
-    path('xml/', show_xml, name='show_xml'),
-    path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
-    path('json/', show_json, name='show_json'),
-    path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
-    path('register/', register, name='register'),
-    path('login/', login_user, name='login'),
-    path('logout/', logout_user, name='logout'),
+    path("", views.show_wishlist, name="show_wishlist"),
+    path("xml/", views.show_xml, name="show_xml"),
+    path("xml/<int:id>/", views.show_xml_by_id, name="show_xml_by_id"),
+    path("json/", views.show_json, name="show_json"),
+    path("json/<int:id>/", views.show_json_by_id, name="show_json_by_id"),
+    path("register/", views.register, name="register"),
+    path("login/", views.login_user, name="login"),
+    path("logout/", views.logout_user, name="logout"),
+    path("ajax/", views.show_wishlist_ajax, name="ajax"),
+    path("ajax/submit", views.add_wishlist_ajax, name="ajax"),
 ]
